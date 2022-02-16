3ef3b05b77adbc3841fe4f03d27608c343417bae
B_1E10_32 <sample@bths.edu>
2022-02-16 10:35:49 -0500

    Finished the lab

--- src/SpellChecker.java
+++ src/SpellChecker.java
@@ -1,6 +1,7 @@
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.Arrays;
 
@@ -18,6 +19,7 @@ public class SpellChecker
   {
     return dictionary;
   }
+
 
   /** This uses LINEAR search to find a word in the dictionary ArrayList and also
     * prints out the number of words checked.
@@ -52,9 +54,34 @@ public class SpellChecker
   */
   public boolean binarySpellCheck(String word)
   {
-    /* IMPLEMENT ME! */
+    int numCheck = 0;
+    int left = 0;
+    int right = dictionary.size() - 1;
 
-    return false; // STUB
+    while (left <= right)
+    {
+      int middle = (left + right) / 2;
+
+      if (word.compareTo(dictionary.get(middle)) < 0)
+      {
+        right = middle - 1;
+        numCheck++;
+      }
+      else if (word.compareTo(dictionary.get(middle)) > 0)
+      {
+        left = middle + 1;
+        numCheck++;
+      }
+      else
+      {
+        numCheck++;
+        System.out.println("-- BINARY SEARCH: Number of words checked (loops/runtime): " + numCheck);
+        return true;
+      }
+    }
+
+    System.out.println("-- BINARY SEARCH: Number of words checked (loops/runtime): " + numCheck);
+    return false;
   }
 
   // private helper method, called in the constructor, which loads the words
@@ -64,7 +91,7 @@ public class SpellChecker
     String[] tmp = null;
     try
     {
-      FileReader fileReader = new FileReader("src\\dictionary.txt");
+      FileReader fileReader = new FileReader("src\\AudreyDictionary.txt");
       BufferedReader bufferedReader = new BufferedReader(fileReader);
       ArrayList<String> lines = new ArrayList<String>();
       String line = null;
